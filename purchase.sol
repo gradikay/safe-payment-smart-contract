483a34ca37630af854924fd0d4bc859d366555be
GRADI M KAYAMBA <56418645+gradikay@users.noreply.github.com>
2021-09-11 20:32:42 -0500

    Update purchase.sol

--- purchase.sol
+++ purchase.sol
@@ -35,7 +35,7 @@ contract Purchase is Context {
     address payable public founder; // Founder of the contract.
     address payable public deposit; // Deposit address for ehter fee.
     uint256 public fee;             // Fee for using the contract
-    // escrow[][][]       : uses the seller's and buyer's address as a reference.
+    // escrow[][][]       : Uses the seller's and buyer's address as a reference.
     mapping(uint256 => mapping(address => mapping(address => uint256))) escrow;
     // locked[][][]       : Locks transaction on the order based on the transaction id.
     mapping(uint256 => mapping(address => mapping(address => bool))) locked;
